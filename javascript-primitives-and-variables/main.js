b471ef537b6de15d7d98d80ee61f4026bd4308fc
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-01-31 10:23:16 -0800

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Jacob Yi';
+console.log('Fullname =', fullName);
+
+var isCool = true;
+console.log('Cool?', isCool);
+
+var totalPets = 0;
+console.log('number of pets:', totalPets);
+
+console.log('type of fullName:', typeof fullName);
+console.log('type of isCool:', typeof isCool);
+console.log('type of totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('type of unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('type of never:', typeof never);
