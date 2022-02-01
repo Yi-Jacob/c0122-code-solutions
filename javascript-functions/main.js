58cba8521068fd68ac2cced953c6e157389f2b07
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-01 15:06:47 -0800

    completed javascript-functions

---
+++ javascript-functions/main.js
@@ -0,0 +1,36 @@
+function convertMinutesToSeconds(minutes) {
+  var seconds = minutes * 60;
+  return seconds;
+}
+var secondsResult = convertMinutesToSeconds(6);
+console.log('Seconds:', secondsResult);
+
+function greet(name) {
+  var greeting = 'Hey, ' + name;
+  return greeting;
+}
+var greetingResult = greet('Jacob');
+console.log('Greeting:', greetingResult);
+
+function getArea(width, height) {
+  var area = width * height;
+  return area;
+}
+var areaResult = getArea(10, 10);
+console.log('Area:', areaResult);
+
+function getFirstName(person) {
+  person.firstName = 'Lelouche';
+  person.lastName = 'Lamperouge';
+  var firstNameResult = person.firstName;
+  return firstNameResult;
+}
+var gotFirstName = getFirstName({ firstName: 'Lelouche', lastName: 'Lamperouge' });
+console.log('Firstname:', gotFirstName);
+
+function getLastElement(array) {
+  var lastElement = array[array.length - 1];
+  return lastElement;
+}
+var last = getLastElement(['propane', 'and', 'propane', 'accessories']);
+console.log('Last item:', last);
