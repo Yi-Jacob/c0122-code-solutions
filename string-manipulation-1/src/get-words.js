94ed07c38d6b0a49318f81cd01080dd9adebfdfb
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-04 20:14:45 -0800

    revised get words function

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -9,15 +9,10 @@ make the newly separated characters into a string
 add new strings to the array
 */
 function getWords(string) {
-  var array = [];
   var empty = [];
   var words = string.split(' ');
   if (string === '') {
     return empty;
-  }
-  for (var i = 0; i <= string.length; i++) {
-    string[i] += ' ';
-    array.push(words);
   }
   return words;
 }
