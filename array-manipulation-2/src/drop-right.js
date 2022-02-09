faec0771bb0472084e94637a111200b477129c9b
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-09 10:39:42 -0800

    in progress

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,9 @@
 /* exported dropRight */
+
+function dropRight(array, count) {
+  if (count > array.length) {
+    return array;
+  } else {
+    return array.slice(0, array.length - count);
+  }
+}
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,15 @@
 /* exported takeRight */
+/*
+function with 2 parameters
+takes an array as one argument then a number as the second argument
+'removes' the first elements based off the argument number
+returns the new array
+*/
+
+function takeRight(array, count) {
+  if (count > array.length) {
+    return array;
+  } else {
+    return array.slice(count + 1, array.length - 1);
+  }
+}
