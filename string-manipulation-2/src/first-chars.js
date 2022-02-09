9b7fe22bc2cd95ff18ccbf74dca902b8b220be53
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-08 20:04:45 -0800

    in progress

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,4 +1,13 @@
 /* exported firstChars */
 /*
 function with 2 parameters
+set the length of the string determined by the first argument
+get the number of characters based off the length
+return a new string with the number of characters based off the length
+the new characters should be the first characters based off length
 */
+
+function firstChars(length, string) {
+  var stringLength = length;
+  return string.slice(0, stringLength);
+}
