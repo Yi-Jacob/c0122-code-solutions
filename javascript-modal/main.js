2120064135d1d69d6af5ff833ed5c574b6d054e7
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-10 14:33:48 -0800

    javascript-modal

---
+++ javascript-modal/main.js
@@ -0,0 +1,24 @@
+/*
+var $open = document.querySelector('.open-modal');
+$open.addEventListener('click', popup);
+
+var $no = document.querySelector('.no-button');
+$no.addEventListener('click', toggle);
+
+var $modal = document.querySelector('.modal');
+$modal.addEventListener('click', toggle);
+
+function popup(event) {
+  $modal.className
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+*/
