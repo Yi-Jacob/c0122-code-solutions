c0b249c0ec3bc8eb10c215a3406df3dfe5e4007d
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-10 20:18:26 -0800

    completed javascript-view-swapping

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,30 +1,3 @@
-/*
-var $tabContainer = document.querySelector('.tab-container');
-var $tab = document.querySelector('.tab');
-var $view = document.querySelector('.view');
-
-$tabContainer.addEventListener('click', check);
-
-function check(event) {
-  event.target.matches('.tab');
-  var target = event.target.getAttribute('data-view');
-
-  for (var i = 0; i < $tab.length; i++) {
-    if (event.target === $tab[i]) {
-      $tab[i].classList.add('active');
-      $view[i].classList.add('active');
-    } else {
-      $tab[i].classList.remove('active');
-      $view[i].classList.remove('active');
-    }
-  }
-
-  for (var i = 0, i < $view.length; i++) {
-
-  }
-}
-*/
-
 var $tab = document.querySelectorAll('.tab');
 
 var $tabContainer = document.querySelector('.tab-container');
