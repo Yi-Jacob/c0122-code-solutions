e84086a85701387bd6eb4eecede76cc625690c7b
Jacob Yi <96788421+jacobyi11@users.noreply.github.com>
2022-02-11 08:56:55 -0800

    completed array-manipulation-2

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,4 +1,11 @@
 /* exported dropRight */
+/*
+function with 2 parameters
+check the length of the array and make sure its less than the count
+if its less then you can just return the array
+if its not cut off the right part of the array
+retrun the array starting at the first index up to the count
+*/
 
 function dropRight(array, count) {
   if (count > array.length) {
