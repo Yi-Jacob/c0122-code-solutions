9a3b97a601b6f9bebe62926a7d2ff46e8177a97e
jacobyi11 <jakeyi00@gmail.com>
2022-02-24 20:35:33 -0800

    completed interview practice

--- practice-interview/main.js
+++ practice-interview/main.js
@@ -1,42 +1,21 @@
 function getRangeReport(start, end) {
-  this.start = start;
-  this.end = end;
-}
-
-getRangeReport.prototype.getTotal = function (start, end) {
-  return this.start + this.end;
-};
-
-getRangeReport.prototype.getOdds = function () {
-  var odds = [];
-  if (this.start % 2 !== 0) {
-    odds.push(this.start);
-  } else if (this.end % 2 !== 0) {
-    odds.push();
-  }
-  return odds;
-};
-
-getRangeReport.prototype.getEvens = function () {
-  var evens = [];
-  if (this.start % 2 === 0) {
-    evens.push(this.start);
-  } else if (this.end % 2 === 0) {
-    evens.push(this.end);
-  }
-  return evens;
-};
-
-getRangeReport.prototype.getRange = function () {
+  var total = 0;
   var range = [];
-  range.push(this.start);
-  range.push(this.end);
-  range.sort();
-  return range;
-};
-
-getRangeReport.prototype.getAverage = function () {
-  var average = 0;
-  average = this.getTotal() / 2;
-  return average;
-};
+  for (var i = start; i <= end; i++) {
+    range.push(i);
+    total = total + i;
+  }
+  var odds = [];
+  var evens = [];
+  for (i = start; i <= end; i++) {
+    if (i % 2 === 0) {
+      evens.push(i);
+    } else odds.push(i);
+  }
+  var average = total / ((end - start) + 1);
+  var report = {
+    total, odds, evens, range, average
+  };
+  return report;
+}
+getRangeReport();
