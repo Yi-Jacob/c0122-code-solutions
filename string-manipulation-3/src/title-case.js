42f4ec5be56fc5d16e671081dff6cfabfc18a99f
jacobyi11 <jakeyi00@gmail.com>
2022-02-25 09:52:37 -0800

    in progress

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -10,9 +10,36 @@ Javascript should return JavaScript
 */
 
 function titleCase(title) {
-  var words = title.split(' ');
-  for (var i = 0; i < title.length; i++) {
-    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  var lowerCased = title.toLowerCase();
+  var lowerCasedList = ['and', 'or', 'nor', 'but', 'a', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'the'];
+  var words = lowerCased.split(' ');
+  words[0] = words[0].charAt(0).toUpperCase() + words[0].substring(1);
+  if (words[0].charAt(0) === 'J') {
+    words[0] = 'JavaScript:';
+    words[1] = 'The';
+  }
+  for (var i = 1; i < words.length; i++) {
+    if (lowerCasedList.indexOf(words[i]) === -1) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
+    }
+    if (words[i].indexOf(':') !== -1) {
+      words[i + 1] = words[i + 1].charAt(0).toUpperCase() + words[i + 1].substring(1);
+    }
+    if (words[i].indexOf('-') !== -1) {
+      var location = words[i].indexOf('-');
+      var upperCaseLetter = words[i].charAt(location + 1).toUpperCase();
+      var newWord = words[i].substring(0, location + 1) + upperCaseLetter + words[i].substring(location + 2, words[i].length);
+      words[i] = newWord;
+    }
+    if (words[i] === 'Javascript') {
+      words[i] = 'JavaScript';
+    }
+    if (words[i] === 'Javascript:') {
+      words[i] = 'JavaScript:';
+    }
+    if ((words[i] === 'Api')) {
+      words[i] = 'API';
+    }
   }
   return words.join(' ');
 }
