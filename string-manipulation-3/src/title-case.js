83c47aed96705be9705ef12671521fd60c00794f
jacobyi11 <jakeyi00@gmail.com>
2022-03-01 04:37:10 +0000

    completed stringstring-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,17 +1,21 @@
 /* exported titleCase */
 /*
 function with one parameter
-takes a string of an argument
-separates each individual word in the string
-checks the length of each word
-if its 4 or more capitlizie the first letter EXCEPT for API
-if the 3 letter or less word is in the front of the string then capitilize
-Javascript should return JavaScript
+lowercase the full string to make it easier to capitalize
+add all the special case words into an array to hold onto
+get each individual word by splitting it
+upper case the first letter of each word and add the rest of the letters of that word
+change Javascript to JavaScript
+loop through the words
+-if its a special case you uppercase it
+-if it has a colon or dash you upper case the word prior
+if its api return API
+put the words together by the spaces they had
 */
 
 function titleCase(title) {
   var lowerCased = title.toLowerCase();
-  var lowerCasedList = ['and', 'or', 'nor', 'but', 'a', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'the'];
+  var specialCases = ['and', 'or', 'nor', 'but', 'a', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'the'];
   var words = lowerCased.split(' ');
   words[0] = words[0].charAt(0).toUpperCase() + words[0].substring(1);
   if (words[0].charAt(0) === 'J') {
@@ -19,7 +23,7 @@ function titleCase(title) {
     words[1] = 'The';
   }
   for (var i = 1; i < words.length; i++) {
-    if (lowerCasedList.indexOf(words[i]) === -1) {
+    if (specialCases.indexOf(words[i]) === -1) {
       words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
     }
     if (words[i].indexOf(':') !== -1) {
