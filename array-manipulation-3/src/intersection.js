413a653b3e4bc29a696505c158d3ae576336ca12
jacobyi11 <jakeyi00@gmail.com>
2022-03-07 03:34:44 +0000

    in progress

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,25 @@
 /* exported intersection */
+/*
+function with 2 parameters
+new array to store the return value
+loop through the first array
+if the item is in the second array push it to the new array
+loop through the seconf array
+if the item is in the first array push it to the new array
+return the new array
+*/
+
+function intersection(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      newArray.push(first[i]);
+    }
+  }
+  for (var x = 0; x < second.length; x++) {
+    if (first.includes(second[i])) {
+      newArray.push(second[x]);
+    }
+  }
+  return newArray;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,25 @@
 /* exported union */
+/*
+function with 2 parameters
+new array to store the values
+loop through the first array
+if the push all the items of the first array into the new array
+loop through the second array
+push all the items into the new array that are NOT in the first array
+return the new array
+*/
+
+function union(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (!(newArray.includes(first[i]))) {
+      newArray.push(first[i]);
+    }
+  }
+  for (var x = 0; x < second.length; x++) {
+    if (!(newArray.includes(second[x]))) {
+      newArray.push(second[x]);
+    }
+  }
+  return newArray;
+}
