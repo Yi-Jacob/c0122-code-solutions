c14dca9884361daa2b0e4ad2a20fa9cda8306ef3
jacobyi11 <jakeyi00@gmail.com>
2022-03-07 03:46:41 +0000

    in progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,20 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+// var players = [
+//   {
+//     name: player1,
+//     hand: []
+//   },
+//   {
+//     name: player2,
+//     hand: []
+//   },
+//   {
+//     name: player3,
+//     hand: []
+//   },
+//   {
+//     name: player4,
+//     hand: []
+//   }
+// ];
