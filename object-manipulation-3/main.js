4ad7c2be68b339629f56e7be14c46f965b619e43
jacobyi11 <jakeyi00@gmail.com>
2022-03-10 19:08:23 -0800

    in progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,20 +1,39 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-// var players = [
-//   {
-//     name: player1,
-//     hand: []
-//   },
-//   {
-//     name: player2,
-//     hand: []
-//   },
-//   {
-//     name: player3,
-//     hand: []
-//   },
-//   {
-//     name: player4,
-//     hand: []
-//   }
-// ];
+/*
+var players = [
+  {
+    name: player1,
+    hand: []
+  },
+  {
+    name: player2,
+    hand: []
+  },
+  {
+    name: player3,
+    hand: []
+  },
+  {
+    name: player4,
+    hand: []
+  }
+];
+
+var deck = [];
+var suit = ['spades', 'hearts', 'clubs', 'diamonds'];
+var rank = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 'Jack', 'Queen', 'King'];
+
+function shuffleDeck() {
+  var cards = {};
+  for (var i = 0; i < suit.length; i++) {
+    for (var x = 0; x < rank.length; x++) {
+      cards.suit = suit[i];
+      cards.rank = rank[x];
+      deck.push(cards);
+      cards = {};
+    }
+  }
+}
+shuffleDeck();
+*/
