c52f4ac50a7f27091052a3bd6f9f3fa806c708ec
jacobyi11 <jakeyi00@gmail.com>
2022-03-16 17:45:45 +0000

    completed node-module-system exercise Jacob Yi

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('Type of:', typeof exports);
+console.log('Value:', exports);
+
+console.log('Type of:', typeof require);
+console.log('Value:', require);
+
+console.log('Type of:', typeof module);
+console.log('Value:', module);
+
+console.log('Type of:', typeof __filename);
+console.log('Value:', __filename);
+
+console.log('Type of:', typeof __dirname);
+console.log('Value:', __dirname);
