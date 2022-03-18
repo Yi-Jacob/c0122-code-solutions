153b6ed6eb2d149f5c966b463d03730909078dda
jacobyi11 <jakeyi00@gmail.com>
2022-03-18 00:30:06 +0000

    completed node-notes-cli exercise Jacob Yi

--- node-notes-cli/main.js
+++ node-notes-cli/main.js
@@ -3,6 +3,7 @@ const jsonFile = require('./data.json');
 
 const command = process.argv[2];
 const content = process.argv[3];
+const update = process.argv[4];
 
 if (command === 'read') {
   for (const values in jsonFile.notes) {
@@ -12,6 +13,14 @@ if (command === 'read') {
   jsonFile.notes[jsonFile.nextId] = content;
   jsonFile.nextId++;
   write(jsonFile);
+} else if (command === 'delete') {
+  delete jsonFile.notes[content];
+  write(jsonFile);
+} else if (command === 'update') {
+  jsonFile.notes[content] = update;
+  write(jsonFile);
+} else {
+  console.log('please use a valid method');
 }
 
 function write(data) {
