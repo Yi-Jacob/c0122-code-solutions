ebeee4d20d7eb740f2e30fe3c94676411bc5b9c8
jacobyi11 <jakeyi00@gmail.com>
2022-03-20 00:24:15 +0000

    in progress

---
+++ express-post-json/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+let nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const object in grades) {
+    gradesArray.push(grades[object]);
+  }
+  res.json(gradesArray);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  const data = req.body;
+  // eslint-disable-next-line no-console
+  console.log('Name: ', data.name);
+  // eslint-disable-next-line no-console
+  console.log('Course: ', data.course);
+  // eslint-disable-next-line no-console
+  console.log('Score: ', data.score);
+
+  data.id = nextId;
+  grades[nextId] = data;
+  nextId++;
+  res.status('201');
+  res.send(data);
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('listening on port 3000!');
+});
