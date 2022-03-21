1926519432aacf9114b0cf5f2a24e808354281b7
jacobyi11 <jakeyi00@gmail.com>
2022-03-21 23:39:05 +0000

    in progress, completed post feature

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-// const fs = require('fs');
+const fs = require('fs');
 const app = express();
+const dataFile = require('./data.json');
+const notes = dataFile.notes;
+const updatedFile = dataFile;
+const newNotes = updatedFile.notes;
+let nextId = updatedFile.nextId;
+const expressJson = express.json();
 
-const dataJSon = require('./data.json');
-const notes = dataJSon.notes;
+app.use(express.json());
 
 // Clients can GET a list of notes.
 app.get('/api/notes', (req, res) => {
@@ -27,7 +32,37 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
+// Clients can POST a new note.
+app.post('/api/notes', (req, res) => {
+  const newContent = req.body;
+  const id = nextId;
+  if (newContent.content === undefined) {
+    res.status(400).json({ error: 'No content provided!' });
+  } else {
+    newNotes[id] = newContent;
+    newContent.id = id;
+    dataFile.nextId++;
+    nextId++;
+    const updatedNotes = JSON.stringify(updatedFile, null, 2);
+    fs.writeFile('data.json', updatedNotes, err => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'An unexpected error occured.' });
+      } else {
+        res.status(201).json(newContent);
+      }
+    });
+  }
+});
+
+// Clients can DELETE a note by id.
+app.delete('/api/notes/:id', (req, res) => {
+
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on Port 3000');
 });
+
+app.use(expressJson);
