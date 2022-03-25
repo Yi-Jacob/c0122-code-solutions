d9a01a1ed107ec4b706609db824b402e518527b8
jacobyi11 <jakeyi00@gmail.com>
2022-03-25 00:28:36 +0000

    in progress sgt-back-end completed post method

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -13,7 +13,7 @@ const db = new pg.Pool({
 });
 
 // Feature 1
-app.get('/api/grades', (req, res, next) => {
+app.get('/api/grades', (req, res) => {
   const sql = `
     select *
       from "grades"
@@ -23,20 +23,48 @@ app.get('/api/grades', (req, res, next) => {
       const grade = result.rows;
       res.status(200).json(grade);
     })
-    .catch(err => {
-      console.error(err);
+    .catch(error => {
+      console.error(error);
       res.status(500).json({
         error: 'An unexpected error occurred.'
       });
     });
 });
 
-// app.post('/api/grades', (req, res, next) => {
-//   const sql = `
-//   insert into "grades" ("name", "course", "score")
-//   values
-//   `
-// });
+// Feature 2
+app.post('/api/grades', (req, res, next) => {
+  const newName = req.body.name;
+  const newCourse = req.body.course;
+  const newScore = req.body.score;
+  const sql = `
+  insert into "grades" ("name", "course", "score")
+  values ($1, $2, $3)
+  returning *
+  `;
+  const grade = [newName, newCourse, newScore];
+  if ((!newName) || (!newCourse) || (!newScore)) {
+    res.status(400).json({
+      error: 'Please include all 3 fields'
+    });
+    return;
+  } else if (newScore < 0 || newScore > 100) {
+    res.status(400).json({
+      error: 'Please use an integer between 0 and 100'
+    });
+    return;
+  }
+  db.query(sql, grade)
+    .then(result => {
+      const newGrade = result.rows[0];
+      res.status(201).json(newGrade);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
