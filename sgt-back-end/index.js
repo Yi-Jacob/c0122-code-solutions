87fb9ad45dbedb42ba322877547a731d4b6e48c2
jacobyi11 <jakeyi00@gmail.com>
2022-03-25 03:48:08 +0000

    in progress sgt-back-end completed feature 3

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -66,6 +66,45 @@ app.post('/api/grades', (req, res, next) => {
     });
 });
 
+// Feature 3
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = Number(req.params.gradeId);
+  const name = req.body.name;
+  const course = req.body.course;
+  const score = parseInt(req.body.score);
+  const sql = `
+    UPDATE "grades"
+    SET "name" = $1,
+        "course" = $2,
+        "score" = $3
+    WHERE "gradeId" = ${gradeId}
+    RETURNING *
+    `;
+  const values = [name, course, score];
+
+  if (!name || !course || !score) {
+    res.status(400).json({ error: 'Please include all 3 fields' });
+    return;
+  } else if (score < 0 || score > 100) {
+    res.status(400).json({ error: 'Please use an integer between 0 and 100' });
+    return;
+  }
+
+  db.query(sql, values)
+    .then(result => {
+      const updatedGrade = result.rows[0];
+      if (!updatedGrade) {
+        res.status(404).json({ error: `gradeId ${gradeId} does not exist in grades table!` });
+      } else {
+        res.status(200).json(updatedGrade);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on Port 3000!');
