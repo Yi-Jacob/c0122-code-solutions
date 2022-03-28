442d2b75abcaa1cf76f36f4c8f59f90cbfaed1d3
jacobyi11 <jakeyi00@gmail.com>
2022-03-28 03:20:26 +0000

    mempool api testing

--- test-api/main.js
+++ test-api/main.js
@@ -1,43 +1,33 @@
 var $list = document.querySelector('#user-list');
-var city = 'huntington beach';
+// var city = 'huntington beach';
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.openbrewerydb.org/breweries?by_city=' + city);
+xhr.open('GET', 'https://mempool.space/api/address/3Bs39s2zNpoTU6sbUTaKXdaU8PuPXm2koN');
 xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
   // console.log('XHR Status: ', xhr.status);
   // console.log('XHR Response: ', xhr.response);
-  for (var i = 0; i < xhr.response.length; i++) {
 
-    var $listItem = document.createElement('li');
-    $listItem.textContent = xhr.response[i].name;
-    $list.appendChild($listItem);
+  var $listItem = document.createElement('li');
+  $listItem.textContent = `Search Address ${xhr.response.address}`;
+  $list.appendChild($listItem);
 
-    var $sublist = document.createElement('ul');
-    $listItem.appendChild($sublist);
+  var $sublist = document.createElement('ul');
+  $listItem.appendChild($sublist);
 
-    var $addy = document.createElement('li');
-    $addy.textContent = xhr.response[i].street;
-    $sublist.appendChild($addy);
+  var $addy = document.createElement('li');
+  var funds = xhr.response.chain_stats.funded_txo_sum / 1000000000;
+  $addy.textContent = `Total amount of Bitcoin: ${funds}`;
+  $sublist.appendChild($addy);
 
-    var $type = document.createElement('li');
-    $type.textContent = 'Brewery Type: ' + xhr.response[i].brewery_type;
-    $sublist.appendChild($type);
+  var $type = document.createElement('li');
+  $type.textContent = 'Transaction Total ' + xhr.response.chain_stats.tx_count;
+  $sublist.appendChild($type);
 
-    var $websiteUrl = document.createElement('li');
-    $websiteUrl.textContent = '';
-    $sublist.appendChild($websiteUrl);
-
-    var $anchor = document.createElement('a');
-    if (xhr.response[i].website_url !== null) {
-      $anchor.setAttribute('href', xhr.response[i].website_url);
-      $anchor.textContent = 'Website';
-      $websiteUrl.appendChild($anchor);
-    } else {
-      $anchor.textContent = 'No Website available';
-      $websiteUrl.appendChild($anchor);
-    }
-  }
+  // var $websiteUrl = document.createElement('li');
+  // $websiteUrl.textContent = '';
+  // $sublist.appendChild($websiteUrl);
 }
+
 );
 xhr.send();
