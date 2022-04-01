ba3e8fda930a45147de75350fb4c84ab6a3b4423
jacobyi11 <jakeyi00@gmail.com>
2022-04-01 20:25:37 +0000

    in progress

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,29 +3,62 @@ import React from 'react';
 class stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      isClicked: false
-    };
+    this.state = ({ isRunning: false, seconds: 0 });
+
     this.handleClick = this.handleClick.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.handlePause = this.handlePause.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   handleClick() {
-    this.setState(prevState => (
-      {
-        isClicked: !prevState.isClicked
-      }
-    ));
+    this.setState({
+      isRunning: !this.state.isRunning
+    });
+    this.startTimer();
+  }
+
+  startTimer() {
+    const { isRunning } = this.state;
+
+    if (!isRunning) {
+      this.tick = setInterval(() => {
+        this.setState({ seconds: this.state.seconds + 1 });
+      }, 1000);
+    }
+  }
+
+  handlePause() {
+    const { isRunning } = this.state;
+    if (isRunning) {
+      clearInterval(this.tick);
+      this.setState({ isRunning: false });
+    }
+  }
+
+  reset() {
+    this.setState({ seconds: 0 });
+    clearInterval(this.tick);
   }
 
   render() {
+    const isRunning = this.state.isRunning;
+    const seconds = this.state.seconds;
+    let icon;
+    if (!isRunning) {
+      icon = <i onClick={this.handleClick} className="fas fa-play"></i>;
+    } else {
+      icon = <i onClick={this.handlePause} className="fas fa-pause"></i>;
+    }
+
     return (
       <>
-      <div className="circle">
-        test
-      </div>
-      <button onClick={this.handleClick}>
-          {this.state.isClicked ? <i className="fa-solid fa-pause fa-3x"></i> : <i className="fa-solid fa-play fa-3x"></i>}
-      </button>
+        <div onClick={this.reset} className='circle'>
+          <span className='time'>{seconds}</span>
+        </div>
+        <div>
+          {icon}
+        </div>
       </>
     );
   }
