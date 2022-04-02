23908b9030d36e7297cee55e95d5401e2775468f
jacobyi11 <jakeyi00@gmail.com>
2022-04-02 22:14:57 +0000

    completed react-app-drawer-component Jacob Yi

--- react-app-drawer-component/src/index.jsx
+++ react-app-drawer-component/src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Drawer from './app-drawer';
+const root = document.getElementById('root');
 ReactDOM.render(
   <Drawer />,
-  document.getElementById('root')
+  root
 );
