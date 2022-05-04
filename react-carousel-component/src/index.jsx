eb52e014dd971a783bb39d4ad5fae8657708da5c
jacobyi11 <jakeyi00@gmail.com>
2022-05-04 01:18:54 +0000

    completed carousel

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -4,23 +4,23 @@ import Carousel from './carousel';
 
 const images = [
   {
-    url: 'https://www.smashbros.com/assets_v2/img/fighter/mario/main8.png',
+    url: '../images/001.png',
     id: 1
   },
   {
-    url: 'https://ssb.wiki.gallery/images/thumb/b/bb/Luigi_SSBU.png/1200px-Luigi_SSBU.png',
+    url: '../images/004.png',
     id: 2
   },
   {
-    url: 'https://ssb.wiki.gallery/images/thumb/0/04/Wario_SSBU.png/1200px-Wario_SSBU.png',
+    url: '../images/007.png',
     id: 3
   },
   {
-    url: 'https://ssb.wiki.gallery/images/b/b9/WaluigiSMP2.png',
+    url: '../images/025.png',
     id: 4
   },
   {
-    url: 'https://ssb.wiki.gallery/images/thumb/8/8d/Yoshi_SSBU.png/1200px-Yoshi_SSBU.png',
+    url: '../images/039.png',
     id: 5
   }
 ];
