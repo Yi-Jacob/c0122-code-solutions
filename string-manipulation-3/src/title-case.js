cc235da22b1b5aa9c383d36431e9bfe26afe0bfe
jacobyi11 <jakeyi00@gmail.com>
2022-05-04 01:38:52 +0000

    revsed

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -13,39 +13,6 @@ if its api return API
 put the words together by the spaces they had
 */
 
-// function titleCase(title) {
-//   var lowerCased = title.toLowerCase();
-//   var specialCases = ['and', 'or', 'nor', 'but', 'a', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'the'];
-//   var words = lowerCased.split(' ');
-//   words[0] = words[0].charAt(0).toUpperCase() + words[0].substring(1);
-
-//   for (var i = 1; i < words.length; i++) {
-//     if (specialCases.indexOf(words[i]) === -1) {
-//       words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
-//     }
-//     if (words[i].indexOf(':') !== -1) {
-//       words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
-//     }
-//     if (words[i].indexOf('-') !== -1 || words[i].indexOf(':') !== -1) {
-//       var location = words[i].indexOf('-');
-//       var upperCaseLetter = words[i].charAt(location + 1).toUpperCase();
-//       var newWord = words[i].substring(0, location + 1) + upperCaseLetter + words[i].substring(location + 2, words[i].length);
-//       words[i] = newWord;
-//     }
-//     if (words[i] === 'Javascript') {
-//       words[i] = 'JavaScript';
-//     }
-//     if (words[i] === 'Javascript:') {
-//       words[i] = 'JavaScript:';
-//       words[i + 1] = words[i + 1].charAt(0).toUpperCase() + words[i + 1].substring(1);
-//     }
-//     if (words[i] === 'Api') {
-//       words[i] = 'API';
-//     }
-//   }
-//   return words.join(' ');
-// }
-
 function titleCase(title) {
   var word = title.toLowerCase().split(' ');
   var minor = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
