419e1af2c1ecdb6ce82fd93241cc8fff27f1a1a0
jacobyi11 <jakeyi00@gmail.com>
2022-05-06 15:00:19 -0700

    fetch promise.all practice

--- fetch/main.js
+++ fetch/main.js
@@ -8,17 +8,24 @@
 //   .then(data => console.log(data))
 //   .catch(err => console.log('Fetch failed!', err));
 
-Promise.all([
-  fetch('https://jsonplaceholder.typicode.com/users'),
-  fetch('https://pokeapi.co/api/v2/pokemon/1')
-]).then(async ([aa, bb]) => {
-  const a = await aa.json();
-  const b = await bb.json();
-  return [a, b];
-})
-  .then(responseText => {
-    console.log(responseText);
+// Promise.all([
+//   fetch('https://jsonplaceholder.typicode.com/users'),
+//   fetch('https://pokeapi.co/api/v2/pokemon/1')
+// ]).then(async ([aa, bb]) => {
+//   const a = await aa.json();
+//   const b = await bb.json();
+//   return [a, b];
+// })
+//   .then(responseText => {
+//     console.log(responseText);
 
-  }).catch(err => {
-    console.log(err);
-  });
+//   }).catch(err => {
+//     console.log(err);
+//   });
+
+Promise.all([
+  fetch('https://jsonplaceholder.typicode.com/users').then(resp => resp.json().then(console.log)),
+  fetch('https://pokeapi.co/api/v2/pokemon/1').then(resp => resp.json()),
+  fetch('https://jsonplaceholder.typicode.com/todos/3').then(resp => resp.json())
+]);
+// .then(console.log);
