f31ae778487813e75758d43d2289929f758d2853
Yi-Jacob <jakeyi00@gmail.com>
2022-05-15 13:34:27 -0700

    ip

--- fetch/main.js
+++ fetch/main.js
@@ -1,7 +1,22 @@
-fetch('https://blockchain.info/rawaddr/bc1q8ea6w5tvs458u7r6u5n88ggzhkk0hmedpgnu9z')
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.log('Fetch failed!', err));
+import mempoolJS from '@mempool/mempool.js';
+
+const init = async () => {
+
+  const { bitcoin: { addresses } } = mempoolJS({
+    hostname: 'mempool.space'
+  });
+
+  const address = '1wiz18xYmhRX6xStj2b9t1rwWX4GKUgpv';
+  const myAddress = await addresses.getAddress({ address });
+  console.log(myAddress);
+
+};
+init();
+
+// fetch('https://blockchain.info/rawaddr/bc1q8ea6w5tvs458u7r6u5n88ggzhkk0hmedpgnu9z')
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+//   .catch(err => console.log('Fetch failed!', err));
 
 // fetch('https://pokeapi.co/api/v2/pokemon/1')
 //   .then(res => res.json())
